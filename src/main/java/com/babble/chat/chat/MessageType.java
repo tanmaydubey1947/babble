4532d3871058f5d3f435d1b52c2b092a702ef7c9
Tanmay Dubey <tanmaydubey1947@gmail.com>
2023-12-10 11:25:38 +0530

    Addded babble chat code

---
+++ src/main/java/com/babble/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.babble.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/test/java/com/main/babble/BabbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.babble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
